4d6fc8509c9027bd43d85c8a2a68605104ba5ff3
Sarah Zimmer <Sarah Zimmer@DESKTOP-Q3NRSDA.wifi.uc.edu>
2022-11-10 14:29:09 -0500

    Added request for nps.gov parkinglots data and parsed data.

--- Group12_Assignment11/MainPackage/MainModule.py
+++ Group12_Assignment11/MainPackage/MainModule.py
@@ -10,8 +10,22 @@ Anything else that's relevant:
 '''
 import jsons
 import requests
+from _ast import Compare
 
-response = requests.get('')
+response = requests.get('https://developer.nps.gov/api/v1/parkinglots?parkCode=acad&api_key=Ig9AOzpzV54Y7qfMzRkRjCvI4UlnuMjoin11586P')
 json_string = response.content
     
 parsed_json = jsons.loads(json_string)
+#print(parsed_json)
+
+totalLots = parsed_json['total']
+print("There are " + totalLots + " parking lots in this park.")
+
+data = list(parsed_json['data'])
+print(data)
+'''   
+spaces = ()
+for totalSpaces in parsed_json['data']:
+    print(sum(int(totalSpaces)))
+'''
+
